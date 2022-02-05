de8fc866c895b60e29ea322e7669b2ef758d6987
R. Nick Vandemark <rnvandemark@gmail.com>
2022-02-04 20:40:59 -0500

    Give discovery server the 'backup' option.

--- bin/start_discovery_server.sh
+++ bin/start_discovery_server.sh
@@ -2,4 +2,4 @@
 # the server ID can be overridden, otherwise it's 0
 # the IP address of the server can be overridden, but is the first IP of this host
 # the server port can be overridden, but is 11811 by default
-fastdds discovery -i "${SERVER_ID:-0}" -l "${SERVER_IP:-$(hostname -I | awk '{ print $1 }')}" -p "${SERVER_PORT:-11811}"
+fastdds discovery -i "${SERVER_ID:-0}" -l "${SERVER_IP:-$(hostname -I | awk '{ print $1 }')}" -p "${SERVER_PORT:-11811}" -b
